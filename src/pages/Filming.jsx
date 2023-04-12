947c82a178b8e9752f51fccbd5c439451a0acd77
youjin <w2117@e-mirim.hs.kr>
2023-04-13 06:57:56 +0900

    타이머 추가

--- src/pages/Filming.jsx
+++ src/pages/Filming.jsx
@@ -1,17 +1,45 @@
 import React, { useState } from 'react';
 import Webcam from 'react-webcam';
-import { Button, Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import styled from 'styled-components';
 
 function Filming() {
   const [imageSrcs, setImageSrcs] = useState([]);
+  const [timeLeft, setTimeLeft] = useState(8);
 
   const webcamRef = React.useRef(null);
+  const timeRef = React.useRef(Date.now());
 
   const capture = React.useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
     setImageSrcs(prevImageSrcs => [...prevImageSrcs, imageSrc]);
   }, [webcamRef, setImageSrcs]);
+
+  React.useEffect(() => {
+    const intervalId = setInterval(() => {
+      const elapsed = (Date.now() - timeRef.current) / 1000;
+      const newTimeLeft = Math.max(0, 8 - elapsed);
+      setTimeLeft(newTimeLeft);
+      if (newTimeLeft === 0) {
+        clearInterval(intervalId);
+        timeRef.current = Date.now();
+      }
+    }, 100);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      if (imageSrcs.length < 8) {
+        capture();
+        setTimeLeft(8);
+        timeRef.current = Date.now();
+      }
+    }, timeLeft * 1000);
+
+    return () => clearTimeout(timer);
+  }, [imageSrcs, capture, timeLeft]);
 
   const saveImage = imageSrc => {
     const canvas = document.createElement('canvas');
@@ -32,22 +60,17 @@ function Filming() {
   return (
     <Container>
       <Row>
+        <div>{Math.round(timeLeft)} seconds left</div>
         <Content>
           <Col>
             <Webcam
               width="853px"
-              heigth="485px"
+              height="485px"
               audio={false}
               ref={webcamRef}
               screenshotFormat="image/jpeg"
               className="w-100 h-100"
             />
-            <br />
-            <Button onClick={() => {
-              for (let i = 0; i < 8; i++) {
-                setTimeout(capture, i * 1000);
-              }
-            }}>Capture photos automatically</Button>
           </Col>
         </Content>
         <Col>
@@ -61,11 +84,12 @@ function Filming() {
               />
             </div>
           ))}
+          <br />
         </Col>
       </Row>
     </Container>
   );
-};
+}
 
 const Content = styled.div`
   display: flex;
@@ -76,5 +100,5 @@ const Content = styled.div`
   height: 60vh;
   margin-top: 20vw;
 `;
+
 export default Filming;
-  
