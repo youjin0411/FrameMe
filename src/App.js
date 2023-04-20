b95fdba80f4c6edd11c06717f18d939c5fbc4c45
youjin <w2117@e-mirim.hs.kr>
2023-04-20 11:54:47 +0900

    경로 수정 router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // BroweRouter as Router는 라우터를 사용할 때 사용하는 컴포넌트로 라우터를 사용할 때는 무조건 이 컴포넌트를 사용해야 한다.
 // 터미널에서 모듈 설치 : npm install react-router-dom
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Navbar from './navbar/index';
 import Main from "./pages/Mainstart";
 import Wepcam from "./pages/Filming";
@@ -12,15 +12,25 @@ function App() {
     return (
       // 라우터는 하나의 컴포넌트로 감싸야 한다. 
       // 감싸지 않으면 라우터를 사용할 수 없다. 
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/filming" element={<Wepcam />} />
-          <Route path="/choiceimg" element={<ChoiceImg />} />
-          <Route path="/image-page" element={<Imgpage />} />
-        </Routes>
-      </Router>
+ <Router>
+      <Routes>
+        <Route path="/" element={<>
+          <Main />
+        </>} />
+        <Route path="/filming" element={<>
+          <Navbar />
+          <Wepcam />
+        </>} />
+        <Route path="/choiceimg" element={<>
+          <Navbar />
+          <ChoiceImg />
+        </>} />
+        <Route path="/image-page" element={<>
+          <Navbar />
+          <Imgpage />
+        </>} />
+      </Routes>
+    </Router>
     );
 }
 
