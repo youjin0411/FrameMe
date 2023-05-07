b1747f39d16e1d4f8729e42d971a80315acea2a8
Kim-yujin <w2117@e-mirim.hs.kr>
2023-05-07 23:32:48 +0900

    test

--- src/pages/filters.js
+++ src/pages/filters.js
@@ -5,6 +5,7 @@ export function grayscaleFilter(pixels) {
         var g = d[i + 1];
         var b = d[i + 2];
         // 흑백값을 더 밝게 만들기 위해 0.6을 더해준다.
+        // 조금 밝은색의 흑백
         var v = 0.2126 * r + 0.7152 * g + 0.0722 * b; // 보정값 변경
         d[i] = d[i + 1] = d[i + 2] = v + 10; // RBG 색을 같게 맞추고, 적절한 값을 더하여 회색 느낌을 살려준다.
     }
