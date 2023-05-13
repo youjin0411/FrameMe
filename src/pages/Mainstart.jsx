41065a8214e7899536108df08f3c94afb19d2945
Kim-yujin <w2117@e-mirim.hs.kr>
2023-05-13 21:20:14 +0900

    edit

--- src/pages/Mainstart.jsx
+++ src/pages/Mainstart.jsx
@@ -8,10 +8,7 @@ import image2 from '../img/whitelogo.png'
 import mainimg from '../img/Mainbg.png'
 
 function Mainstart(){
-   
    return (
-	  // 사진도 가윤이가 추가했을 것 같아서 !! backgroundColor로 일단 대처해서 코드 작성했어 ! 여기에 이제 backgroundColor대신 backgrondImage 사용하면 돼 ! ex) backgroundImage: `url(${Imagepage})` 
-	  // 여기서 Imagepage => 위에서 import Imagepage from "경로"
 	  <body style={{height: '100%', width: '100%',position: 'absolute', backgroundSize: 'cover', backgroundImage: `url(${mainimg})`}}>
 		 <Container>
 		 <Nav>
@@ -92,7 +89,6 @@ const Logo = styled(Link)`
 const Blank = styled.div`
    width: 45vw;
 `
-
 const NavMenu = styled.div`
 display: flex;
 justify-content: space-evenly;
