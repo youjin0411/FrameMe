08eb13e7f1a25a29216293b7728b1d8253c5aa6a
youjin <w2117@e-mirim.hs.kr>
2023-06-02 13:33:06 +0900

    photos

--- src/pages/Photoshoot.jsx
+++ src/pages/Photoshoot.jsx
@@ -50,11 +50,57 @@ const ImageEditor = (props) => {
     }
   };
 
+  // Image Choice
+  const [selectedImages, setSelectedImages] = useState([]);
+
+  useEffect(() => {
+    const storedImages = localStorage.getItem('selectedImages');
+    if (storedImages) {
+      setSelectedImages(JSON.parse(storedImages));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('selectedImages', JSON.stringify(selectedImages));
+  }, [selectedImages]);
+
+  const handleImageClick = (image) => {
+    let updatedSelectedImages;
+    if (selectedImages.length < 4) {
+      updatedSelectedImages = [...selectedImages, image];
+    } else {
+      updatedSelectedImages = [...selectedImages.slice(1), image];
+    }
+    setSelectedImages(updatedSelectedImages);
+  };
+
+  const renderDivBoxes = () => {
+    const divBoxes = [];
+    for (let i = 0; i < 4; i++) {
+      const selectedImage = selectedImages[i % selectedImages.length];
+      const backgroundImage = selectedImage ? `url(${selectedImage})` : 'none';
+
+      divBoxes.push(
+        <div
+          key={i}
+          style={{
+            width: '200px',
+            height: '200px',
+            border: '1px solid black',
+            backgroundImage,
+            backgroundSize: 'cover',
+          }}
+        />
+      );
+    }
+    return divBoxes;
+  };
+
   return (
     <div>
       <canvas 
       ref={canvasRef} 
-      width="245" height="158" />
+      width="245" height="158" onClick={() => handleImageClick(canvasRef)}/>
       {/* <br /> 
         <div style={{display:'grid', gridTemplateColumns:'45px 45px 45px'}}>
             <button onClick={() => { applyFilter(grayscaleFilter) }}>흑백</button>
@@ -73,18 +119,6 @@ const WebcamApp = (props) => {
   const [timeLeft, setTimeLeft] = useState(6);
   const timeRef = useRef(Date.now());
   const webcamRef = useRef(null);
-  const [selectedImages, setSelectedImages] = useState([]);
-
-  useEffect(() => {
-    const storedImages = localStorage.getItem('selectedImages');
-    if (storedImages) {
-      setSelectedImages(JSON.parse(storedImages));
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem('selectedImages', JSON.stringify(selectedImages));
-  }, [selectedImages]);
 
   const capture = useCallback(async () => {
     const imageSrc = webcamRef.current.getScreenshot();
@@ -129,42 +163,6 @@ const WebcamApp = (props) => {
   } else {
     return null; // 다른 경로에서는 실행하지 않음
   }
-
-  const handleImageClick = (image) => {
-    console.log("클릭")
-    let updatedSelectedImages;
-    if (selectedImages.length < 4) {
-      updatedSelectedImages = [...selectedImages, image];
-    } else {
-      updatedSelectedImages = [...selectedImages.slice(1), image];
-    }
-    setSelectedImages(updatedSelectedImages);
-
-    // 클릭한 이미지를 localStorage에 저장
-    localStorage.setItem('selectedImages', JSON.stringify(updatedSelectedImages));
-  };
-
-  const renderDivBoxes = () => {
-    const divBoxes = [];
-    for (let i = 0; i < 4; i++) {
-      const selectedImage = selectedImages[i % selectedImages.length];
-      const backgroundImage = selectedImage ? `url(${selectedImage})` : 'none';
-
-      divBoxes.push(
-        <div
-          key={i}
-          style={{
-            backgroundImage,
-            backgroundSize: 'cover',
-            width:219.98, 
-            height:140.77, left:1043.56, top:300.68,background:'#ffffff', marginTop:20, marginLeft:38.56
-          }}
-        />
-      );
-    }
-    return divBoxes;
-  };
-
   if (showResult) {
         return (
             <>
@@ -172,7 +170,7 @@ const WebcamApp = (props) => {
             <div style={{margin:'0 auto', background:'white', width: 1820, height:900, left: 50, top: 180, backgroundBlendMode: 'overlay', borderRadius: '30px 30px 0px 0px', boxShadow:'0px 0px 2px 2px #F5F5F5', marginTop:65}}>
                 <div style={{position:'absolute', width:196, height:60, left:1633,top:249, borderRadius: 30, background:'#white',backgroundBlendMode: 'overlay', boxShadow:'0px 0px 2px 2px #F5F5F5'}}></div>
                 <div style={{display:'grid', gridTemplateColumns:'245px 245px', marginLeft:328, marginTop:100, position:'absolute', gridColumnGap: 10}}>
-                    {images.map((i, index) => <ImageEditor key={index} imageSrc={i} style={{background:'#000000'}}  onClick={() => handleImageClick(i)} />)}
+                    {images.map((i, index) => <ImageEditor key={index} imageSrc={i} style={{background:'#000000'}} />)}
                 </div>
                 {/* 9.39 */}
                 <div style={{position:'absolute', width:583, height:683, left:105, top:259, background:'#000000',marginLeft:900, marginTop:60,}}>
@@ -182,9 +180,9 @@ const WebcamApp = (props) => {
                         <div style={{width:219.98, height:140.77, left:1043.56, top:300.68, background:'#ffffff', marginLeft:38.56}}></div>
                         <div style={{width:219.98, height:140.77, left:1043.56, top:300.68, background:'#ffffff', marginLeft:38.56}}></div>
                     </div> */}
-                    <div >
+                    {/* <div >
                       {renderDivBoxes()}
-                    </div>
+                    </div> */}
                 </div>
             </div>
                 {/* <button onClick={() => {
