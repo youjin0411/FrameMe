e346e8b97fb9e1abb1d8c0daf9fe4c50a21c06f2
youjin <w2117@e-mirim.hs.kr>
2023-06-07 12:29:27 +0900

    주석추가

--- src/pages/Photoshoot.jsx
+++ src/pages/Photoshoot.jsx
@@ -2,24 +2,48 @@ import React, { useEffect, useRef, useState, useCallback } from 'react';
 import ReactDOM from 'react-dom/client';
 import Webcam from 'react-webcam';
 
+// 비디오 콘테이너
 const videoConstraints = {
   width: 1001,
   height: 641,
   facingMode: 'user',
 };
 
+
 const WebcamApp = () => {
   const maxCount = 8;
   const [count, setCount] = useState(0);
+  // 촬영 후 이미지를 보여주는지 여부 
   const [showResult, setShowResult] = useState(false);
+  // 촬영한 사진 배열
   const [images, setImages] = useState([]);
+  // 6초 촬영 타이머
   const [timeLeft, setTimeLeft] = useState(6);
+  // 6초 감소 시킬 timeRef
   const timeRef = useRef(Date.now());
+  // webcam 
   const webcamRef = useRef(null);
+
+  // localstorage에 넣을 이미지 배열을 담을 변수
   const [selectedImages, setSelectedImages] = useState([]);
 
+  //localStorage에서 "selectedImages"라는 키로 저장된 데이터를 가져오기
+  useEffect(() => {
+    const storedImages = localStorage.getItem('selectedImages');
+    if (storedImages) {
+      setSelectedImages(JSON.parse(storedImages));
+    }
+  }, []);
+
+  // selectedImages 배열이 변경될 때마다 실행
+  useEffect(() => {
+    localStorage.setItem('selectedImages', JSON.stringify(selectedImages));
+  }, [selectedImages]);
+
+  // 촬영한 사진 클릭 시 localstorage에 넣기
   const handleImageClick = (image) => {
     let updatedSelectedImages;
+    // localstorage에 넣을 배열 추가
     if (selectedImages.length < 4) {
       updatedSelectedImages = [...selectedImages, image];
     } else {
@@ -28,9 +52,11 @@ const WebcamApp = () => {
     setSelectedImages(updatedSelectedImages);
   };
   
+  // 프레임 위의 박스 이미지 출력 코드 
   const renderDivBoxes = () => {
     const divBoxes = [];
     for (let i = 0; i < 4; i++) {
+      // selectedImages의 배열들을 순차대로 이미지 넣기 
       const selectedImage = selectedImages[i % selectedImages.length];
       const backgroundImage = selectedImage ? `url(${selectedImage})` : 'none';
       divBoxes.push(
@@ -51,15 +77,18 @@ const WebcamApp = () => {
     return divBoxes;
   };  
 
+  // 캡쳐 함수 
   const capture = useCallback(async () => {
     const imageSrc = webcamRef.current.getScreenshot();
     setImages((imgs) => imgs.concat(imageSrc));
     setCount((c) => {
+      // 0부터 8이 되기 전까지 돌아보기 
       if (c === maxCount - 1) setShowResult(true);
       return c + 1;
     });
   }, [webcamRef, maxCount]);
 
+  // 타임에 맞추어 타이머 돌리기 
   useEffect(() => {
     const intervalId = setInterval(() => {
       setTimeLeft((prevTimeLeft) => {
@@ -76,6 +105,7 @@ const WebcamApp = () => {
     return () => clearInterval(intervalId);
   }, [capture]);
 
+  // 8장 이하로 촬영하기 
   useEffect(() => {
     const timer = setTimeout(() => {
       if (images.length < 8) {
@@ -88,6 +118,7 @@ const WebcamApp = () => {
     return () => clearTimeout(timer);
   }, [images, capture, timeLeft]);
 
+  // photoshoot 경로에서만 실행되게 하기 
   const pathname = window.location.pathname;
   if (pathname.includes('/photoshoot')) {
     // '/photoshoot' 경로 또는 '/photoshoot'을 포함한 경로에서 실행하는 코드 작성
